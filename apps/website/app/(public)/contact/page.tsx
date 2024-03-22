3b9b617bc618a8b51548e1660ed731ee4ff0e9ff
Ania <kujawanna@gmail.com>
2024-03-22 12:22:44 +0100

    optimization and reviews fetching

--- apps/website/app/(public)/contact/page.tsx
+++ apps/website/app/(public)/contact/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
 import { Header } from "@ems/common-ui";
 
 const ContactPage = () => {
   return (
-    <Header>Contact</Header>
+    <div>
+      <Header>Contact</Header>
+      <Image src="/nachos.png" width={600} height={400} alt="Nachos" />
+    </div>
   )
 }
 
--- apps/website/app/(public)/reviews/page.tsx
+++ apps/website/app/(public)/reviews/page.tsx
@@ -1,7 +1,21 @@
-const Reviews = () => {
-  return (
-    <h1>Reviews</h1>
-  )
-}
+import { Header } from '@ems/common-ui';
+import { fetchReviews } from './services';
 
-export default Reviews;
+export default async function ReviewsPage() {
+  const reviews = await fetchReviews();
+  return (
+    <div>
+      <Header>Reviews</Header>
+      <p>All reviews</p>
+      <ul>
+        {reviews?.map((elem) => (
+          <li key={elem.id}>
+            <div>{elem.content}</div>
+            <div className="font-light">{elem.author}</div>
+            <div className="mb-4 font-light">{elem.created_at}</div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
