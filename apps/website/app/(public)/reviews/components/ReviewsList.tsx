97e05a66ea334820d1d5724562e8f35cf0ddeae2
Ania <kujawanna@gmail.com>
2024-04-19 14:03:30 +0200

    add streaming to reviews page

---
+++ apps/website/app/(public)/reviews/components/ReviewsList.tsx
@@ -0,0 +1,18 @@
+import { fetchReviews } from '../services';
+
+
+export default async function ReviewsList() {
+  const reviews = await fetchReviews();
+
+  return (
+    <ul>
+      {reviews?.map((elem) => (
+        <li key={elem.id}>
+          <div>{elem.content}</div>
+          <div className="font-light">{elem.author}</div>
+          <div className="mb-4 font-light">{elem.created_at}</div>
+        </li>
+      ))}
+    </ul>
+  );
+}
--- apps/website/app/(public)/reviews/page.tsx
+++ apps/website/app/(public)/reviews/page.tsx
@@ -1,24 +1,24 @@
 import { Header } from '@ems/common-ui';
-import { fetchReviews } from './services';
 import Link from 'next/link';
+import { Suspense } from 'react';
+
+import ReviewsCount from './components/ReviewsCount';
+import ReviewsList from './components/ReviewsList';
 
 export default async function ReviewsPage() {
-  // const reviews = await fetchReviews();
-  const reviews = [];
   return (
     <div>
       <Header>Reviews</Header>
       <p>All reviews</p>
       <Link href='/reviews/create' className='my-2 text-blue-600'>Create review</Link>
-      <ul>
-        {reviews?.map((elem) => (
-          <li key={elem.id}>
-            <div>{elem.content}</div>
-            <div className="font-light">{elem.author}</div>
-            <div className="mb-4 font-light">{elem.created_at}</div>
-          </li>
-        ))}
-      </ul>
+
+      <Suspense fallback={<p>Loading count...</p>}>
+        <ReviewsCount />
+      </Suspense>
+
+      <Suspense fallback={<p>Loading reviews...</p>}>
+        <ReviewsList />
+      </Suspense>
     </div>
   );
 }
